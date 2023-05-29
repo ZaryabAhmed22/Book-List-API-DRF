4f735292cb188fb2893581356e9639ffc246290a
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-05-29 20:20:47 +0500

    Adding a functional view for example

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BookView
+from . import views
 
 urlpatterns = [
     path("books", BookView.as_view())
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -9,3 +11,12 @@ from rest_framework import generics
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# Using functional views for serialization
+
+
+@api_view()
+def books(request):
+    books = Book.objects.all()
+    serialized_item = BookSerializer(books, many=True)
+    return Response(serialized_item.data)
