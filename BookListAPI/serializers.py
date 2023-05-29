a1380d8f00c8b899ccb67985a34e8c3cbc687e39
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-05-29 20:27:49 +0500

    Adding new field based on method

--- BookListAPI/serializers.py
+++ BookListAPI/serializers.py
@@ -1,8 +1,15 @@
 from .models import Book
 from rest_framework import serializers
+from decimal import Decimal
 
 
 class BookSerializer(serializers.ModelSerializer):
+    price_after_tax = serializers.SerializerMethodField(
+        method_name="calculate_tax")
+
     class Meta:
         model = Book
-        fields = ["id", "author", "price", "title"]
+        fields = ["id", "author", "price", "title", "price_after_tax"]
+
+    def calculate_tax(self, product: Book):
+        return product.price * Decimal(1.1)
