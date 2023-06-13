59efba00fe6cc3e484703f50f677dd2397813734
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-06-13 19:51:02 +0500

    Adding a function view for fingle book

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -3,5 +3,6 @@ from .views import BookView
 from . import views
 
 urlpatterns = [
-    path("books", BookView.as_view())
+    path("books", BookView.as_view()),
+    path("books/<int:book_id>", views.single_book_view)
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -15,9 +16,19 @@ class BookView(generics.ListCreateAPIView):
 # Using functional views for serialization
 
 
-# Adding the api_view decorator to tell Django that it's an api view and passed an array of allowed HTTP verbs/methods
+# Adding the api_view decorator to tell Django that it's an api view and passed an array of allowed HTTP verbs/methods. The combination of api_view decorator and Response method enables the api view interface on the browser.
 @api_view(['GET', 'POST'])
 def books(request):
     books = Book.objects.all()
     serialized_item = BookSerializer(books, many=True)
-    return Response(serialized_item.data)
+    return Response(serialized_item.data, status=status.HTTP_200_OK)
+
+
+@api_view()
+def single_book_view(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        serialized_item = BookSerializer(book)
+        return Response(serialized_item.data, status=status.HTTP_200_OK)
+    except:
+        return Response({"message": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
